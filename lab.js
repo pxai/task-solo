aaf087f11210f553d98364b1ed0c5e14327e1ba5
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-09-03 21:15:25 +0200

    lab.js playground

--- lab.js
+++ lab.js
@@ -1,10 +1,11 @@
-const Console = require("src/console");
+const Console = require("./src/console");
 const console = new Console();
 
 const answer = console.read("Tell me something"); 
 
 console.read("And something else:").then( data => {
   console.log("You added: ", data);
+  console.log("And answer again: ", answer);
 });
 
 console.log("Ansser:", answer);
