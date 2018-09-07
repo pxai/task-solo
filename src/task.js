079f401e7a6e9bd6fe49b9ab6508f4911e3bfb52
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-09-07 18:36:16 +0200

    Added more attributes

--- src/task.js
+++ src/task.js
@@ -2,6 +2,9 @@ class Task {
   constructor (todo) {
     this._todo = todo;
     this._id = Math.round(Math.random());
+    this._status = 0;
+    this._createdAt = new Date();
+    this._changedAt = null;
   }
 
   get id () {
@@ -10,6 +13,18 @@ class Task {
 
   get todo () {
     return this._todo;
+  }
+
+  get status () {
+    return this._status;
+  }
+
+  get createdAt () {
+    return this._createdAt;
+  }
+
+  get changedAt () {
+    return this._changedAt;  
   }
 }
 
--- test/task.spec.js
+++ test/task.spec.js
@@ -11,6 +11,13 @@ describe("Task Object", () => {
     const task = new Task("Take over the world");
 
     expect(task).to.be.an.instanceof(Task);
+  });
+
+  it("should have initial data", () => {
+    const task = new Task("Important task");
+
     expect(task.id).to.match(/[0-9]+/);
+    expect(task.todo).to.equal("Important task");
+    expect(task.status).to.equal(0);
   });
 });
