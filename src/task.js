91ab94affc8b95e482f4c773839f6846dd3277c5
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-09-07 09:20:33 +0200

    Ok, everything works

--- src/task.js
+++ src/task.js
@@ -1,3 +1,16 @@
-class Task {}
+class Task {
+  constructor (todo) {
+    this._todo = todo;
+    this._id = Math.round(Math.random());
+  }
+
+  get id () {
+    return this._id;
+  }
+
+  get todo () {
+    return this._todo;
+  }
+}
 
 module.exports = Task;
--- test/task.spec.js
+++ test/task.spec.js
@@ -6,4 +6,11 @@ describe("Task Object", () => {
   it("exists a task object",() => {
     expect(Task).to.exist;  
   });
+
+  it("should have a constructor", () => {
+    const task = new Task("Take over the world");
+
+    expect(task).to.be.an.instanceof(Task);
+    expect(task.id).to.match(/[0-9]+/);
+  });
 });
