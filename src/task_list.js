28f719fbd22d6b84879f3261466123b8168b4ba9
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-09-10 10:01:41 +0200

    Added task list management

--- src/task_list.js
+++ src/task_list.js
@@ -1,10 +1,28 @@
 class TaskList {
-  constructor () {
+  constructor (name = "") {
+    this._name = name;
     this._tasks = []; 
   }
 
   get length () {
     return this._tasks.length;
+  }
+
+  get name () {
+    return this._name;  
+  }
+
+  taskAt (index) {
+    return this._tasks[index];
+  }
+  
+  add (task) {
+    this._tasks.push(task);
+  }
+
+  remove (taskId) {
+    let tasks = this._tasks.filter(task => task.id !== taskId);
+    this._tasks = tasks;
   }
 }
 
--- test/task_list.spec.js
+++ test/task_list.spec.js
@@ -1,9 +1,40 @@
 const chai = require('chai');
 const expect = chai.expect;
 const TaskList = require("../src/task_list");
+const Task = require("../src/task");
 
 describe("Task List", () => {
   it("should exist", () => {
     expect(TaskList).to.exist; 
   });   
+
+  it("should have a constructor with defaults", () => {
+    const taskList = new TaskList();
+
+    expect(taskList.length).to.equal(0);
+    expect(taskList.name).to.equal("");
+  });
+
+  it("should be able to set a name", () => {
+    const taskList = new TaskList("MyList");
+
+    expect(taskList.name).to.equal("MyList");
+  });
+
+ context("List Task item management", () => {
+  let taskList;
+
+  beforeEach(() => {
+    taskList = new TaskList();
+  });
+
+  it("should add elements correctly", () => {
+    expect(taskList.length).to.equal(0);
+
+    taskList.add(new Task("Do something"));
+
+    expect(taskList.length).to.equal(1);
+  });
+
+ });
 });
