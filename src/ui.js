f051f84f3066509e3d583a3e67350a33e29477e7
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-10-06 12:54:28 +0200

    added test for console.log

--- src/ui.js
+++ src/ui.js
@@ -1,6 +1,10 @@
 class UI {
     constructor () {
     }  
+
+    start () {
+        console.log("Task Solo");
+    }
 }
 
 module.exports = UI;
--- test/ui.spec.js
+++ test/ui.spec.js
@@ -1,6 +1,8 @@
 const chai = require("chai");
+const sinon = require("sinon");
 const expect = chai.expect;
 const UI = require("../src/ui");
+require("mocha-sinon");
 
 describe("UI Object", () => {
   it("exists a ui object",() => {
@@ -10,4 +12,17 @@ describe("UI Object", () => {
   it("should have a constructor", () => {
     const ui = new UI();
   });
- });
+  
+  describe("Output", () => {
+    beforeEach(() => {
+        sinon.stub(console, "log").callsFake( () => log.apply(log, arguments));
+    });  
+
+    it("should show a message when started", () => {
+        const ui = new UI(); 
+        ui.start();
+        
+        expect(console.log.calledOnce).to.be.true;
+    });
+  });
+});
