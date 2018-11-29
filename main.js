ebf060508b3653e54855786e3acc0651b1df7e21
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-11-29 23:59:10 +0100

    await looks better

--- main.js
+++ main.js
@@ -4,8 +4,16 @@ const Console = require("./src/console");
 const input = new Console();
 // const ui = new UI();
 
-const answer = await input.read("say something");
-console.log("you wrote: ", answer);
+// const answer = await input.read("say something");
+// input.read("jejeje").then( res => { console.log(res); });
+
+async function main () {
+  let res = await input.read("jejeje");
+  console.log(res);
+}
+
+main();
+// console.log("you wrote: ", answer);
 
 // ui.start();
 
